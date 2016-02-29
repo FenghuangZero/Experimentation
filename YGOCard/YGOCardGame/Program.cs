f2e4d45b25f0c744595fa46a9f9c81378a586340
FenghuangZero <christopher.paul.martin@hotmail.com>
2016-02-29 16:19:59 +0000

    Made a start.

--- YGOCard/YGOCardGame/Program.cs
+++ YGOCard/YGOCardGame/Program.cs
@@ -12,6 +12,11 @@ namespace YGOCardGame
         {
             Console.WriteLine("Hello World");
 
+            Card blueEyes = new Card("Blue-Eyes White Dragon", "This legendary dragon is a powerful engine of destruction. Virtually invincible, very few have faced this awesome creature and lived to tell the tale.", 89631139, "Dragon");
+
+
+            Console.WriteLine(blueEyes.getName());
+            Console.WriteLine(blueEyes.getDesc());
             // Keep the console window open in debug mode.
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
