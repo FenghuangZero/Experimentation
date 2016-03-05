9be86c4383b25ea74bbc4b60d1f0139f4a6b8b8a
FenghuangZero <christopher.paul.martin@hotmail.com>
2016-03-05 16:53:05 +0000

    Changed console output of card name and descriptions to loop.

--- YGOCard/YGOCardGame/Program.cs
+++ YGOCard/YGOCardGame/Program.cs
@@ -17,10 +17,14 @@ namespace YGOCardGame
             deck[0] = new Card("Blue-Eyes White Dragon", "This legendary dragon is a powerful engine of destruction. Virtually invincible, very few have faced this awesome creature and lived to tell the tale.", 89631139, "Dragon");
             //deck[0].setName("Red-Eyes Black Dragon");
             deck[1] = new Card("Dark Magician", "The ultimate wizard in terms of attack and defense.", 46986414, "Spellcaster");
-            Console.WriteLine(deck[0].getName());
-            Console.WriteLine(deck[0].getDesc());
-            Console.WriteLine(deck[1].getName());
-            Console.WriteLine(deck[1].getDesc());
+
+            // Describe cards in deck.
+            for (int i = 0; i < deck.Length; i++)
+            {
+                Console.WriteLine(deck[i].getName());
+                Console.WriteLine(deck[i].getDesc());
+            }
+
             // Keep the console window open in debug mode.
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
