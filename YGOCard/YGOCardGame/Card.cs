bc6d75b73df04daa2de6cfeeb4ccca41e0141141
FenghuangZero <christopher.paul.martin@hotmail.com>
2016-03-07 10:34:15 +0000

    Fleshed out card objects constructors and methods.

--- YGOCard/YGOCardGame/Card.cs
+++ YGOCard/YGOCardGame/Card.cs
@@ -22,18 +22,33 @@ namespace YGOCardGame
             CardNumber = 0;
             Type = "";
         }
-        public Card(string cardName, string desc, int num, string type)
+        public Card(string cardName, string cardDesc, int cardNum, string cardType)
         {
             this.Name = cardName;
             Image = "";
-            this.Description = desc;
-            this.CardNumber = num;
-            this.Type = type;
+            this.Description = cardDesc;
+            this.CardNumber = cardNum;
+            this.Type = cardType;
         }
 
         public void setName(String cardName)
         {
             Name = cardName;
+        }
+
+        public void setDesc(String cardDesc)
+        {
+            Description = cardDesc;
+        }
+
+        public void setNum(int cardNum)
+        {
+            CardNumber = cardNum;
+        }
+
+        public void setType(string cardType)
+        {
+            Type = cardType;
         }
 
         public string getName()
@@ -43,7 +58,16 @@ namespace YGOCardGame
 
         public string getDesc()
         {
-        return Description;
+            return Description;
+        }
+
+        public int getNum()
+        {
+            return CardNumber;
+        }
+        public string getType()
+        {
+            return Type;
         }
     }
 
@@ -52,10 +76,68 @@ namespace YGOCardGame
         public string Attribute { get; set; }
         public int Attack { get; set; }
         public int Defence { get; set; }
-        public int level { get; set; }
+        public int Level { get; set; }
 
         public MonsterCard()
-        { }
+        {
+            Attribute = "";
+            Attack = 0;
+            Defence = 0;
+            Level = 0;
+        }
+
+        public MonsterCard(string cardName, string cardDesc, int cardNum, string cardType, string cardAttribute, int cardAttack, int cardDefence, int cardLevel)
+        {
+            this.Name = cardName;
+            Image = "";
+            this.Description = cardDesc;
+            this.CardNumber = cardNum;
+            this.Type = cardType;
+            this.Attribute = cardAttribute;
+            this.Attack = cardAttack;
+            this.Defence = cardDefence;
+            this.Level = cardLevel;
+        }
+
+        public void setAttribute(String cardAttribute)
+        {
+            Attribute = cardAttribute;
+        }
+
+        public void setAttack(int cardAttack)
+        {
+            Attack = cardAttack;
+        }
+
+        public void setDefence(int cardDef)
+        {
+            Defence = cardDef;
+        }
+
+        public void setLevel(int cardLevel)
+        {
+            Level = cardLevel;
+        }
+
+        public string getAttribute()
+        {
+            return Attribute;
+        }
+
+        public int getAttack()
+        {
+            return Attack;
+        }
+
+        public int getDefence()
+        {
+            return Defence;
+        }
+        
+        public int getLevel()
+        {
+            return Level;
+        }
     }
 
     public class SpellCard : Card
@@ -65,5 +147,36 @@ namespace YGOCardGame
 
         public SpellCard()
         { }
+
+        public SpellCard(string cardName, string cardDesc, int cardNum, string cardType, string cardProp, string cardEffect)
+        {
+            this.Name = cardName;
+            Image = "";
+            this.Description = cardDesc;
+            this.CardNumber = cardNum;
+            this.Type = cardType;
+            this.Property = cardProp;
+            this.Effect = cardEffect;
+        }
+
+        public void setProperty(string cardProp)
+        {
+            Property = cardProp;
+        }
+
+        public void setEffect(string cardEffect)
+        {
+            Effect = cardEffect;
+        }
+
+        public string getProperty()
+        { 
+            return Property;
+        }
+
+        public string getEffect()
+        {
+            return Effect;
+        }
     }
 }
--- YGOCard/YGOCardGame/Program.cs
+++ YGOCard/YGOCardGame/Program.cs
@@ -12,17 +12,28 @@ namespace YGOCardGame
         {
             Console.WriteLine("Hello World");
 
-            Card[] deck = new Card[2];
+            MonsterCard[] deck = new MonsterCard[2];
 
-            deck[0] = new Card("Blue-Eyes White Dragon", "This legendary dragon is a powerful engine of destruction. Virtually invincible, very few have faced this awesome creature and lived to tell the tale.", 89631139, "Dragon");
+            deck[0] = new MonsterCard();
+            
+            //deck[0].setName("Blue-Eyes White Dragon");
+            deck[0].setDesc("This legendary dragon is a powerful engine of destruction. Virtually invincible, very few have faced this awesome creature and lived to tell the tale.");
+            deck[0].setNum(89631139);
+            deck[0].setType("Dragon");
+            deck[0].setAttribute("Light");
+            deck[0].setAttack(3000);
+            deck[0].setDefence(2500);
+            deck[0].setLevel(8);
             //deck[0].setName("Red-Eyes Black Dragon");
-            deck[1] = new Card("Dark Magician", "The ultimate wizard in terms of attack and defense.", 46986414, "Spellcaster");
+            deck[1] = new MonsterCard("Dark Magician", "The ultimate wizard in terms of attack and defense.", 46986414, "Spellcaster", "Dark", 2500, 2100, 7);
 
             // Describe cards in deck.
             for (int i = 0; i < deck.Length; i++)
             {
                 Console.WriteLine(deck[i].getName());
                 Console.WriteLine(deck[i].getDesc());
+                Console.WriteLine("Attack: " + deck[i].getAttack());
+                Console.WriteLine("Defence: " + deck[i].getDefence());
             }
 
             // Keep the console window open in debug mode.
