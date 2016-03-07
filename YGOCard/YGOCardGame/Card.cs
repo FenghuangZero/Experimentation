2d501a16300061f8dc75da234d8fa6f6f76b4665
FenghuangZero <christopher.paul.martin@hotmail.com>
2016-03-07 12:25:00 +0000

    Removed unnecessary methods and implemented use of universal methods defined in variable declarations.

--- YGOCard/YGOCardGame/Card.cs
+++ YGOCard/YGOCardGame/Card.cs
@@ -11,7 +11,7 @@ namespace YGOCardGame
         public string Name { get; set; }
         public string Image { get; set; }
         public string Description { get; set; }
-        public int CardNumber { get; set; }
+        public int Number { get; set; }
         public string Type { get; set; }
 
         public Card()
@@ -19,7 +19,7 @@ namespace YGOCardGame
             Name = "";
             Image = "";
             Description = "";
-            CardNumber = 0;
+            Number = 0;
             Type = "";
         }
         public Card(string cardName, string cardDesc, int cardNum, string cardType)
@@ -27,47 +27,8 @@ namespace YGOCardGame
             this.Name = cardName;
             Image = "";
             this.Description = cardDesc;
-            this.CardNumber = cardNum;
+            this.Number = cardNum;
             this.Type = cardType;
-        }
-
-        public void setName(String cardName)
-        {
-            Name = cardName;
-        }
-
-        public void setDesc(String cardDesc)
-        {
-            Description = cardDesc;
-        }
-
-        public void setNum(int cardNum)
-        {
-            CardNumber = cardNum;
-        }
-
-        public void setType(string cardType)
-        {
-            Type = cardType;
-        }
-
-        public string getName()
-        {
-            return Name;
-        }
-
-        public string getDesc()
-        {
-            return Description;
-        }
-
-        public int getNum()
-        {
-            return CardNumber;
-        }
-        public string getType()
-        {
-            return Type;
         }
     }
 
@@ -91,52 +52,12 @@ namespace YGOCardGame
             this.Name = cardName;
             Image = "";
             this.Description = cardDesc;
-            this.CardNumber = cardNum;
+            this.Number = cardNum;
             this.Type = cardType;
             this.Attribute = cardAttribute;
             this.Attack = cardAttack;
             this.Defence = cardDefence;
             this.Level = cardLevel;
-        }
-
-        public void setAttribute(String cardAttribute)
-        {
-            Attribute = cardAttribute;
-        }
-
-        public void setAttack(int cardAttack)
-        {
-            Attack = cardAttack;
-        }
-
-        public void setDefence(int cardDef)
-        {
-            Defence = cardDef;
-        }
-
-        public void setLevel(int cardLevel)
-        {
-            Level = cardLevel;
-        }
-
-        public string getAttribute()
-        {
-            return Attribute;
-        }
-
-        public int getAttack()
-        {
-            return Attack;
-        }
-
-        public int getDefence()
-        {
-            return Defence;
-        }
-        
-        public int getLevel()
-        {
-            return Level;
         }
     }
 
@@ -146,37 +67,20 @@ namespace YGOCardGame
         public string Effect { get; set; }
 
         public SpellCard()
-        { }
+        {
+            Property = "";
+            Effect = "";
+        }
 
         public SpellCard(string cardName, string cardDesc, int cardNum, string cardType, string cardProp, string cardEffect)
         {
             this.Name = cardName;
             Image = "";
             this.Description = cardDesc;
-            this.CardNumber = cardNum;
+            this.Number = cardNum;
             this.Type = cardType;
             this.Property = cardProp;
             this.Effect = cardEffect;
-        }
-
-        public void setProperty(string cardProp)
-        {
-            Property = cardProp;
-        }
-
-        public void setEffect(string cardEffect)
-        {
-            Effect = cardEffect;
-        }
-
-        public string getProperty()
-        { 
-            return Property;
-        }
-
-        public string getEffect()
-        {
-            return Effect;
         }
     }
 }
--- YGOCard/YGOCardGame/Program.cs
+++ YGOCard/YGOCardGame/Program.cs
@@ -16,26 +16,26 @@ namespace YGOCardGame
 
             deck[0] = new MonsterCard();
             
-            //deck[0].setName("Blue-Eyes White Dragon");
-            deck[0].setDesc("This legendary dragon is a powerful engine of destruction. Virtually invincible, very few have faced this awesome creature and lived to tell the tale.");
-            deck[0].setNum(89631139);
-            deck[0].setType("Dragon");
-            deck[0].setAttribute("Light");
-            deck[0].setAttack(3000);
-            deck[0].setDefence(2500);
-            deck[0].setLevel(8);
-            //deck[0].setName("Red-Eyes Black Dragon");
+            deck[0].Name = "Blue-Eyes White Dragon";
+            deck[0].Description = "This legendary dragon is a powerful engine of destruction. Virtually invincible, very few have faced this awesome creature and lived to tell the tale.";
+            deck[0].Number = 89631139;
+            deck[0].Type = "Dragon";
+            deck[0].Attribute = "Light";
+            deck[0].Attack = 3000;
+            deck[0].Defence = 2500;
+            deck[0].Level = 8;
+            //deck[0].Name = "Red-Eyes Black Dragon";
             deck[1] = new MonsterCard("Dark Magician", "The ultimate wizard in terms of attack and defense.", 46986414, "Spellcaster", "Dark", 2500, 2100, 7);
 
             // Describe cards in deck.
             for (int i = 0; i < deck.Length; i++)
             {
-                Console.WriteLine(deck[i].getName());
-                Console.WriteLine(deck[i].getDesc());
-                Console.WriteLine("Attack: " + deck[i].getAttack());
-                Console.WriteLine("Defence: " + deck[i].getDefence());
+                Console.WriteLine(deck[i].Name);
+                Console.WriteLine(deck[i].Description);
+                Console.WriteLine("Attack: " + deck[i].Attack);
+                Console.WriteLine("Defence: " + deck[i].Defence);
             }
-
+            
             // Keep the console window open in debug mode.
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
