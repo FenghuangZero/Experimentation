f61c9beee18ed581ddb03d4fc7a8a7de2ecfbc75
FenghuangZero <christopher.paul.martin@hotmail.com>
2016-03-11 12:08:59 +0000

    Added loading cards from XML. May need to change later if it is too memory intensive

--- YGOCard/YGOCardGame/Program.cs
+++ YGOCard/YGOCardGame/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Xml;
+using System.Xml.Linq;
 
 namespace YGOCardGame
 {
@@ -17,44 +17,33 @@ namespace YGOCardGame
             Player player1 = new Player("Player 1");
             Player player2 = new Player("Player 2");
 
-            // XmlTextReader reader = new XmlTextReader("YGOCardDB.xml");
+            XDocument doc = XDocument.Load("YGOCardDB.xml");
 
-            
-            /*
-            foreach (MonsterCard m in deck)
+            var dbName = doc.Descendants("Name");
+            var dbDescription = doc.Descendants("Description");
+            var dbNumber = doc.Descendants("Number");
+            var dbType = doc.Descendants("Type");
+            var dbAttribute = doc.Descendants("Attribute");
+            var dbAttack = doc.Descendants("Attack");
+            var dbDefence = doc.Descendants("Defence");
+            var dbLevel = doc.Descendants("Level");
+
+            // Load cards from XML
+            for (int i = 0; i < 40; i++)
             {
-                deck[m] = new MonsterCard();
-                deck[m].Name = XmlText
+                trunk[i] = new Card();
+                trunk[i].Name = dbName.ElementAt(i).Value;
+                trunk[i].Description = dbDescription.ElementAt(i).Value;
+                trunk[i].Number = Int32.Parse(dbNumber.ElementAt(i).Value);
+                trunk[i].Type = dbType.ElementAt(i).Value;
+                trunk[i].Attribute = dbAttribute.ElementAt(i).Value;
+                trunk[i].Attack = Int32.Parse(dbAttack.ElementAt(i).Value);
+                trunk[i].Defence = Int32.Parse(dbDefence.ElementAt(i).Value);
+                trunk[i].Level = Int32.Parse(dbLevel.ElementAt(i).Value);
             }
-            */
-
-            trunk[0] = new Card();
-            trunk[0].Name = "Tri-Horned Dragon";
-            trunk[0].Description = "An unworthy dragon with three sharp horns sprouting from its head.";
-            trunk[0].Number = 39111158;
-            trunk[0].Type = "Dragon";
-            trunk[0].Attribute = "Dark";
-            trunk[0].Attack = 2850;
-            trunk[0].Defence = 2350;
-            trunk[0].Level = 8;
-            //trunk[0].Name = "Red-Eyes Black Dragon";
-            trunk[1] = new Card("Blue-Eyes White Dragon", "This legendary dragon is a powerful engine of destruction. Virtually invincible, very few have faced this awesome creature and lived to tell the tale.", 89631139, "Dragon", "Light", 3000, 2500, 8);
-            trunk[5] = new Card("Dark Magician", "The ultimate wizard in terms of attack and defense.", 46986414, "Spellcaster", "Dark", 2500, 2100, 7);
 
             player2.Deck[0] = trunk[1];
-
             player1.Deck[0] = trunk[5];
-
-            // Describe cards in deck.
-            /*
-            foreach (MonsterCard m in deck)
-            {
-                Console.WriteLine(m.Name);
-                Console.WriteLine(m.Description);
-                Console.WriteLine("Attack: " + m.Attack);
-                Console.WriteLine("Defence: " + m.Defence);
-            }
-            */
 
             //Mock Duel
             player1.draw();
