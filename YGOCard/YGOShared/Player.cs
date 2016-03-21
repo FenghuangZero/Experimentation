2d3a7cc60faa4be18a95c776d5bc9721500a8d0c
FenghuangZero <christopher.paul.martin@hotmail.com>
2016-03-21 11:40:22 +0000

    Windows implementation

--- YGOCard/YGOShared/Player.cs
+++ YGOCard/YGOShared/Player.cs
@@ -19,7 +19,7 @@ namespace YGOCardGame
         // Methods
         public void draw()
         {
-            Console.WriteLine(this.Name + " drew " + Deck[0].Name);
+            //Console.WriteLine(this.Name + " drew " + Deck[0].Name);
             this.move(Deck, Hand, Deck[0], 0);            
             for (int i = 0; i < (Deck.Length - 1); i++)
             {
@@ -32,14 +32,14 @@ namespace YGOCardGame
         public void summon(Card[] monster, int indexLocation)
         {
             monster[indexLocation].Position = "Attack";
-            Console.WriteLine(this.Name + " summons " + monster[indexLocation].Name + " in " + monster[indexLocation].Position + " position.");
+            //Console.WriteLine(this.Name + " summons " + monster[indexLocation].Name + " in " + monster[indexLocation].Position + " position.");
             this.move(Hand, MonsterZone, monster[indexLocation], indexLocation);            
         }
 
         public void set(Card[] monster, int indexLocation)
         {
             monster[indexLocation].Position = "Defence";
-            Console.WriteLine(this.Name + " summons " + "a monster in face down" + monster[indexLocation].Position + " position.");
+            //Console.WriteLine(this.Name + " summons " + "a monster in face down" + monster[indexLocation].Position + " position.");
             this.move(Hand, MonsterZone, monster[indexLocation], indexLocation);            
         }
 
@@ -53,33 +53,33 @@ namespace YGOCardGame
             {
                 monster.Position = "Attack";
             }
-            Console.WriteLine(this.Name + " switches " + monster.Name + " to " + monster.Position + "position.");
+            //Console.WriteLine(this.Name + " switches " + monster.Name + " to " + monster.Position + "position.");
         }
 
         public void attackDirectly(Card[] attacker, int attackerIndex, Player opponent)
         {
-            Console.WriteLine(this.Name + " attacks " + opponent.Name + " directly with " + attacker[attackerIndex].Name + ".");
+            //Console.WriteLine(this.Name + " attacks " + opponent.Name + " directly with " + attacker[attackerIndex].Name + ".");
             opponent.LifePoints -= attacker[attackerIndex].Attack;
-            Console.WriteLine(opponent.Name + "'s life points are " + opponent.LifePoints + ".");
+            //Console.WriteLine(opponent.Name + "'s life points are " + opponent.LifePoints + ".");
         }
 
         public void attackMonster(Card[] attacker, int attackerIndex, Player opponent, Card[] defender, int defenderIndex)
         {
-            Console.WriteLine(this.Name + " attacks " + opponent.Name + "'s " + defender[defenderIndex].Name + " with " + attacker[attackerIndex].Name + ".");
+            //Console.WriteLine(this.Name + " attacks " + opponent.Name + "'s " + defender[defenderIndex].Name + " with " + attacker[attackerIndex].Name + ".");
             if (defender[defenderIndex].Position == "Attack")
             {
                 if (attacker[attackerIndex].Attack > defender[defenderIndex].Attack)
                 {
                     int damage = attacker[attackerIndex].Attack - defender[defenderIndex].Attack;
                     opponent.LifePoints -= damage;
-                    Console.WriteLine(opponent.Name + " takes " + damage + " points of damage");
-                    Console.WriteLine(opponent.Name + "'s Life points are " + opponent.LifePoints);
-                    Console.WriteLine(defender[defenderIndex].Name + " was destroyed.");
+                    //Console.WriteLine(opponent.Name + " takes " + damage + " points of damage");
+                    //Console.WriteLine(opponent.Name + "'s Life points are " + opponent.LifePoints);
+                    //Console.WriteLine(defender[defenderIndex].Name + " was destroyed.");
                     opponent.move(opponent.MonsterZone, opponent.Graveyard, defender[defenderIndex], defenderIndex);
                 }
                 else if (attacker[attackerIndex].Attack == defender[defenderIndex].Attack)
                 {
-                    Console.WriteLine("Both monsters were destroyed.");
+                    //Console.WriteLine("Both monsters were destroyed.");
 
                     this.move(this.MonsterZone, this.Graveyard, attacker[attackerIndex], attackerIndex);
                     opponent.move(opponent.MonsterZone, opponent.Graveyard, defender[defenderIndex], defenderIndex);
@@ -88,9 +88,9 @@ namespace YGOCardGame
                 {
                     int damage = defender[defenderIndex].Attack - attacker[attackerIndex].Attack;
                     this.LifePoints -= damage;
-                    Console.WriteLine(this.Name + " takes " + damage + " points of damage");
-                    Console.WriteLine(this.Name + "'s Life points are " + this.LifePoints);
-                    Console.WriteLine(attacker[attackerIndex].Name + " was destroyed.");
+                    //Console.WriteLine(this.Name + " takes " + damage + " points of damage");
+                    //Console.WriteLine(this.Name + "'s Life points are " + this.LifePoints);
+                    //Console.WriteLine(attacker[attackerIndex].Name + " was destroyed.");
                     this.move(this.MonsterZone, this.Graveyard, attacker[attackerIndex], attackerIndex);
                 }
             }
@@ -98,19 +98,19 @@ namespace YGOCardGame
             {
                 if (attacker[attackerIndex].Attack > defender[defenderIndex].Defence)
                 {
-                    Console.WriteLine(defender[defenderIndex].Name + " was destroyed.");
+                    //Console.WriteLine(defender[defenderIndex].Name + " was destroyed.");
                     opponent.move(opponent.MonsterZone, opponent.Graveyard, defender[defenderIndex], defenderIndex);
                 }
                 else if (attacker[attackerIndex].Attack == defender[defenderIndex].Defence)
                 {
-                    Console.WriteLine(defender[defenderIndex].Name + " endured.");
+                    //Console.WriteLine(defender[defenderIndex].Name + " endured.");
                 }
                 else
                 {
                     int damage = defender[defenderIndex].Defence - attacker[attackerIndex].Attack;
                     this.LifePoints -= damage;
-                    Console.WriteLine(this.Name + " takes " + damage + " ponts of damage");
-                    Console.WriteLine(this.Name + "'s Life points are " + this.LifePoints);
+                    //Console.WriteLine(this.Name + " takes " + damage + " ponts of damage");
+                    //Console.WriteLine(this.Name + "'s Life points are " + this.LifePoints);
                 }
             }
         }
--- YGOCard/YGOWindows/MainPage.xaml.cs
+++ YGOCard/YGOWindows/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using YGOCardGame;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +23,40 @@ namespace YGOWindows
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        Card[] trunk = new Card[130];
+        Player player1 = new Player("Player 1");
+        Player player2 = new Player("Player 2");
+        XmlHandler cardDB = new XmlHandler();
+
         public MainPage()
         {
-            this.InitializeComponent();
+            this.InitializeComponent();           
+
+            cardDB.loadXml(trunk);
+
+            player2.Deck[0] = trunk[1];
+            player1.Deck[0] = trunk[5];
+
+            /*
+            //Mock Duel
+            player1.draw();
+            player1.summon(player1.Hand, 0);
+            player1.attackDirectly(player1.MonsterZone, 0, player2);
+            player2.draw();
+            player2.summon(player2.Hand, 0);
+            player2.attackMonster(player2.MonsterZone, 0, player1, player1.MonsterZone, 0);
+            //Console.WriteLine("Player 1 has drawn Exodia. Player 1 Wins.");
+            */
+
+            // Keep the console window open in debug mode.
+            //Console.WriteLine("Press any key to exit.");
+            //Console.ReadKey();
+        }
+
+        private void deckButton_Click(object sender, RoutedEventArgs e)
+        {
+            player1.draw();
+            p1hand1textBlock.Text = player1.Hand[0].Name;
         }
     }
 }
